78bf9fe9173de84fc9df9d25ce246a47fffb223a
cristianoooliveira <73917884+cristianoooliveira@users.noreply.github.com>
2021-12-28 18:37:15 -0300

    Add files via upload

---
+++ Desafio001.py
@@ -0,0 +1,5 @@
+nome = input("Qual o seu primeiro nome? ")
+sobrenome = input("Qual o seu sobrenome? ")
+print ("Seja bem vindo(a)!",nome, sobrenome, ",é um prazer te conhecer!")
+             
+             
